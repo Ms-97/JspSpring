3ffb53b831fc6e466c736900d694ea02e5c9cc9f
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-07-14 17:45:23 +0900

    0714

--- workspace/servlet_basic/src/com/jsp/controller/Gugudan.java
+++ workspace/servlet_basic/src/com/jsp/controller/Gugudan.java
@@ -14,15 +14,21 @@ public class Gugudan extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String danStr = request.getParameter("dan");
+		String gopStr = request.getParameter("gop");
+		
+		int danNum = Integer.parseInt(danStr);
+		int gopNum = Integer.parseInt(gopStr);
+		
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		
-		for(int dan =2; dan<10; dan++) {
+		for(int dan =2; dan<danNum+1; dan++) {
 			//System.out.println(dan+"단");
 			out.println(dan+"단<br/>");
-			for(int gop=1; gop<10; gop++) {
+			for(int gop=1; gop<gopNum+1; gop++) {
 				//System.out.println(dan + " * "+ gop + " = " + (dan * gop));
-				out.println(dan + " * "+ gop + " = " + (dan * gop)+"단<br/>");
+				out.println(dan + " * "+ gop + " = " + (dan * gop)+"<br/>");
 			}
 			//System.out.println();
 			out.println("<br/>");
