4daa4e7e0ff9b73473c777c161438f41fcf0936d
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-07-26 20:29:13 +0900

    0726

--- workspace/JSP_command_bootstrap/src/com/jsp/controller/InitApplicationContextServlet.java
+++ workspace/JSP_command_bootstrap/src/com/jsp/controller/InitApplicationContextServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import com.jsp.context.ApplicationContext;
 import com.jsp.dao.MemberDAO;
 import com.jsp.dao.MemberDAOImpl;
+import com.jsp.datasource.OracleMybatisSqlSessionFactory;
 import com.jsp.service.MemberServiceImpl;
 
 
@@ -18,14 +19,23 @@ public class InitApplicationContextServlet extends HttpServlet {
 		//System.out.println("init process....");
 		
 		Map<String,Object> map = ApplicationContext.getApplicationContext();
-		
 		map.put("memberService",new MemberServiceImpl());
-		map.put("memberDAO",new MemberDAOImpl());
+		map.put("memberDAO", new MemberDAOImpl());
+		map.put("sqlSessionFactory", new OracleMybatisSqlSessionFactory());
 		
 		
 		MemberServiceImpl memberService = (MemberServiceImpl)map.get("memberService");
 		MemberDAO memberDAO = (MemberDAO)map.get("memberDAO");
 		memberService.setMemberDAO(memberDAO);
+		
+		MemberDAOImpl memberDAOImpl = (MemberDAOImpl)map.get("memberDAO");
+		OracleMybatisSqlSessionFactory sqlSessionFactory 
+					= (OracleMybatisSqlSessionFactory)map.get("sqlSessionFactory");
+		memberDAOImpl.setSqlSessionFactory(sqlSessionFactory);
+		
+		
+		
 	}
-
+	
+	
 }
